97948edb084c0698c717eb1ae6f090c868c471ed
Avokoda <dakotamum@gmail.com>
2021-10-26 20:42:20 -0600

    finished day 01

---
+++ day01/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ifstream fin("input.txt");
+    int aNumber;
+    int sum = 0;
+    /* PART A
+    while(fin >> aNumber)
+    {
+       sum += (aNumber / 3) - 2; 
+    }
+    */
+
+   while(fin >> aNumber)
+   {
+       int currentFuel = (aNumber / 3) - 2;
+       sum += currentFuel;
+       while(currentFuel > 5)
+       {
+           currentFuel = (currentFuel / 3) - 2;
+           sum += currentFuel;
+       }
+   }
+
+    std::cout << sum << std::endl;
+    fin.close();
+    return 0;
+}
